bb98cf4b51a86908db79a1a9446be33c9ce0562d
Ashesh Vidyut <asheshvidyut@Ashesh-Vidyuts-MacBook-Air.local>
2023-03-10 22:22:47 +0530

    init

---
+++ app/javascript/components/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AppCSS from 'App.scss';
+
+export default class App extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (<div>
+            <AppBar position="static">
+                <Typography
+                    className={AppCSS.title}
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                        mr: 2,
+                        display: { xs: 'none', md: 'flex' },
+                        fontFamily: 'monospace',
+                        fontWeight: 700,
+                        letterSpacing: '.3rem',
+                        color: 'inherit',
+                        textDecoration: 'none',
+                    }}
+                >
+                    DIRECTSHIFT
+                </Typography>
+            </AppBar>
+        </div>)
+    }
+};
